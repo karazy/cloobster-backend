d1354dee820a546c52b11ec5f543400e921b5cf3
Frederik Reifschneider <reifschneider@karazy.net>
2011-11-29 19:15:34 +0100

    cleanup 10000

--- EatSenseSencha/app/store/CheckInStore.js
+++
@@ -1,8 +0,0 @@
-Ext.create('Ext.data.Store', {
-	model: 'CheckIn',
-	proxy: {
-		type: 'ajax',
-		url: 'http://localhost:8888/restaurant/spot/serg2011',
-		reader: 'json'
-	}
-});
--- EatSenseTestApp/assets/www/app/store/CheckIn.js
+++
@@ -1,5 +0,0 @@
-Ext.define('EatSense.store.CheckIn', {
-    extend  : 'Ext.data.Store',
-    model   : 'EatSense.model.CheckIn',
-    requires: ['EatSense.model.CheckIn'],
-});
