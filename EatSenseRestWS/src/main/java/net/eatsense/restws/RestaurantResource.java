ce818feb4eb499a7f11bf1b2ea53f926360d270d
Nils Weiher <nils@karazy.de>
2012-02-03 10:28:09 +0100

    added datastore index configuration, removed spreadsheet feed test

--- EatSenseRestWS/src/main/java/net/eatsense/restws/RestaurantResource.java
+++ EatSenseRestWS/src/main/java/net/eatsense/restws/RestaurantResource.java
@@ -1,11 +1,7 @@
 package net.eatsense.restws;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Collection;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
@@ -27,9 +23,6 @@ import net.eatsense.representation.CheckInDTO;
 import net.eatsense.representation.MenuDTO;
 import net.eatsense.representation.RestaurantDTO;
 import net.eatsense.util.DummyDataDumper;
- 
-import com.google.appengine.repackaged.org.json.JSONException;
-import com.google.appengine.repackaged.org.json.JSONObject;
 
 import com.google.inject.Inject;
 
@@ -140,33 +133,6 @@ public class RestaurantResource{
 		return menuCtr.getMenus(restaurantId);
 	}
 	
-	@GET
-	@Path("import/{spreadsheetKey}")
-	@Produces("text/html; charset=utf-8")
-	public String getSpreadsheetTest(@PathParam("spreadsheetKey") String spreadsheetKey) throws JSONException
-	{
-		String returnString = "";
-		try { 
-            URL url = new URL("https://spreadsheets.google.com/feeds/worksheets/"+ spreadsheetKey + "/public/basic?alt=json");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-            
-            while ((line = reader.readLine()) != null) {
-                returnString += line;
-            }
-            reader.close();
-            
-            JSONObject spreadsheets = new JSONObject(returnString);
-            return spreadsheets.toString();
-            
-        } catch (MalformedURLException e) {
-            // ...
-        } catch (IOException e) {
-            // ...
-        }
-		return "fail!";
-	}
-
 	@PUT
 	@Path("dummies")
 	public void dummyData() {
