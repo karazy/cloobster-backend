da5e6738d2fa4cf5d918f5b3bc5cb021a0f1a684
Nils Weiher <nils@karazy.de>
2012-03-16 13:00:33 +0100

    changed type of oldestRequest to fix id comparison

--- EatSenseRestWS/src/main/java/net/eatsense/controller/ChannelController.java
+++ EatSenseRestWS/src/main/java/net/eatsense/controller/ChannelController.java
@@ -6,6 +6,8 @@ import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -22,6 +24,12 @@ import net.eatsense.domain.Restaurant;
 import net.eatsense.persistence.RestaurantRepository;
 import net.eatsense.representation.cockpit.MessageDTO;
 
+/**
+ * Sends push messages via the Google Channel Service.
+ * 
+ * @author Nils Weiher
+ *
+ */
 public class ChannelController {
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 	
@@ -38,18 +46,44 @@ public class ChannelController {
 		mapper = new ObjectMapper();
 	}
 	
+	/**
+	 * Create a new message channel for push notification.
+	 * 
+	 * @param restaurantId numeric id of a restaurant
+	 * @param clientId
+	 * @return
+	 */
 	public String createChannel(Long restaurantId, String clientId) {
 		return createChannel(restaurantId, clientId, null);
 	}
 	
+	/**
+	 * Create a new message channel for push notification, with a default timeout of 2h,
+	 * and register the client id in the restaurant.
+	 * 
+	 * @param restaurant datastore object
+	 * @param clientId
+	 * @return the token to send to the client
+	 */
 	public String createChannel(Restaurant restaurant, String clientId) {
 		return createChannel(restaurant, clientId, null);
 	}
 	
+	
+	/**
+	 * Create a new message channel for push notification, with the given timeout.
+	 * 
+	 * @param restaurant datastore object
+	 * @param clientId
+	 * @param timeout in minutes
+	 * @return the token to send to the client
+	 */
 	public String createChannel(Restaurant restaurant, String clientId , Integer timeout) {
 		String token = null;
+		// create a channel id with the format "restaurantId|clientId|timestamp"
+		clientId = restaurant.getId() + "|" + clientId;
+		clientId = clientId + "|" + new Date().getTime();
 		
-		clientId += new Date().getTime();
 		logger.debug("creating channel for channelID: " +clientId);
 		token = (timeout != null)?channelService.createChannel(clientId, timeout):channelService.createChannel(clientId);
 		
@@ -62,16 +96,32 @@ public class ChannelController {
 		return token;
 	}
 	
+	/**
+	 * Create a new message channel for push notification, with the given timeout.
+	 * 
+	 * @param restaurantId
+	 * @param clientId
+	 * @param timeout
+	 * @return the token to send to the client
+	 */
 	public String createChannel(Long restaurantId, String clientId , Integer timeout) {
 		Restaurant restaurant = restaurantRepo.getById(restaurantId);
-				
-		if(restaurant.getChannelIds() == null) {
-			restaurant.setChannelIds(new ArrayList<String>());
-		}	
+		if(restaurant == null)
+			throw new IllegalArgumentException("got unknown restaurantId: " + restaurantId);
 		return createChannel(restaurant, clientId, timeout);
 	}
 	
-	public String sendMessage(String clientId, String type, String action, Object content) throws Exception {
+	/**
+	 * Send a message to the client identified by the client id.
+	 * 
+	 * @param clientId
+	 * @param type
+	 * @param action 
+	 * @param content an object for data serialization to JSON
+	 * @return the complete message string 
+	 * @throws IOException, JsonGenerationException, JsonMappingException 
+	 */
+	public String sendMessage(String clientId, String type, String action, Object content) throws IOException, JsonGenerationException, JsonMappingException  {
 		MessageDTO messageData = new MessageDTO();
 		String messageString;
 		messageData.setAction(action);
@@ -79,11 +129,17 @@ public class ChannelController {
 		
 		messageData.setContent(content);
 		
-		try {
-			messageString = mapper.writeValueAsString(messageData);
-		}  catch (Exception e) {
-			throw e;
-		} 
+			try {
+				messageString = mapper.writeValueAsString(messageData);
+			} catch (JsonGenerationException e) {
+				throw e;
+			} catch (JsonMappingException e) {
+				throw e;
+			} catch (IOException e) {
+				throw e;
+			}
+		if(messageString.length() > 32786)
+			throw new IllegalArgumentException("data package too long, reduce content size");
 		
 		ChannelMessage message = new ChannelMessage(clientId, messageString);
 		channelService.sendMessage(message);
@@ -91,10 +147,86 @@ public class ChannelController {
 		return messageString;
 	}
 	
-	public void sendMessageToAllClients(Long restaurantId, String type, String action, Object content) throws Exception {
+	
+	/**
+	 * Send message with only the given string.
+	 *  
+	 * @param clientId identifying the client(channel) to send to
+	 * @param messageString the data to send with the message, limited to 32 kb as UTF-8 string
+	 * @return
+	 */
+	public ChannelMessage sendMessageRaw(String clientId, String messageString) {
+		ChannelMessage message = new ChannelMessage(clientId, messageString);
+		channelService.sendMessage(message);
+		
+		return message;
+	}
+	
+	/**
+	 * @param restaurantId
+	 * @param type
+	 * @param action
+	 * @param content
+	 * @throws IOException 
+	 * @throws JsonMappingException 
+	 * @throws JsonGenerationException 
+	 */
+	public void sendMessageToAllClients(Long restaurantId, String type, String action, Object content) throws JsonGenerationException, JsonMappingException, IOException  {
 		Restaurant restaurant = restaurantRepo.getById(restaurantId);
+		String lastMessage = "";
 		for (String clientId : restaurant.getChannelIds()) {
-			logger.debug(sendMessage(clientId, type, action, content));
+			lastMessage = sendMessage(clientId, type, action, content);
+		}
+		logger.debug("last message sent: "+ lastMessage);
+	}
+
+	public void handleDisconnected(HttpServletRequest request) {
+		try {
+			unsubscribeClient(channelService.parsePresence(request).clientId());
+		} catch (IOException e) {
+			logger.error("could not parse presence", e);
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void unsubscribeClient(String clientId) {
+		Restaurant restaurant = parseRestaurant(clientId);
+		
+		if(restaurant == null) {
+			throw new IllegalArgumentException("unknown restaurantId encoded in clientId: "+ clientId);
+		}
+		else {
+			if (restaurant.getChannelIds() == null || restaurant.getChannelIds().isEmpty())	{
+				return;
+			}
+				
+			for (String subscribedClientId : restaurant.getChannelIds()) {
+				
+			}
+		}
+			
+		
+	}
+	
+	private Restaurant parseRestaurant(String clientId) {
+		// retrieve the restaurantId appended at the front of the client id seperated with a "|"
+		Long restaurantId = Long.valueOf(clientId.substring(0, clientId.indexOf("|")-1) );
+		
+		Restaurant restaurant = restaurantRepo.getById(restaurantId);
+				
+		return restaurant;
+	}
+
+	public void subscribeToRestaurant(Long restaurantId, String clientId) {
+		
+	}
+	
+	public void handleConnected(HttpServletRequest request) {
+		try {
+			channelService.parsePresence(request);
+		} catch (IOException e) {
+			logger.error("could not parse presence", e);
+			throw new RuntimeException(e);
 		}
 	}
 }
