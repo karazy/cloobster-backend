8f5f890704369f6b0ef138ba94b943d9dc6e5124
Frederik Reifschneider <reifschneider@karazy.net>
2012-03-19 14:21:49 +0100

    fucking file cleanup II

--- eatSenseAppiOS/eatSense/www_/app/store/CheckIn.js
+++
@@ -1,7 +0,0 @@
-Ext.define('EatSense.store.CheckIn', {
-    extend  : 'Ext.data.Store',    
-    requires: ['EatSense.model.CheckIn'],
-    config : {
-    	model   : 'EatSense.model.CheckIn'
-    }
-});
