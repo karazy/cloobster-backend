734cd95652ebc31a1049dddbc946f691ae8815ad
Frederik Reifschneider <reifschneider@karazy.net>
2012-05-19 00:38:11 +0200

    removed newsletter popup on check out

--- EatSenseRestWS/src/main/app/dev/app/controller/Settings.js
+++ EatSenseRestWS/src/main/app/dev/app/controller/Settings.js
@@ -120,41 +120,41 @@ Ext.define('EatSense.controller.Settings', {
     /**
     * Shows a popup to user asking for his email to register for newsletter.
     */
-    registerNewsletterOnLeaving: function() {
-        var me = this,
-            checkInCtr = this.getApplication().getController('CheckIn'),
-            appState = checkInCtr.getAppState(),
-            popup = Ext.create('EatSense.view.NewsletterPopup');
+    // registerNewsletterOnLeaving: function() {
+    //     var me = this,
+    //         checkInCtr = this.getApplication().getController('CheckIn'),
+    //         appState = checkInCtr.getAppState(),
+    //         popup = Ext.create('EatSense.view.NewsletterPopup');
 
-        //setup button handler
-        popup.on({
-            delegate: 'button[action=register]',
-            tap: function() {
-                 //force keyboard to hide, due to a bug in Android 4.0 the textfield is visible above the popup
-                popup.down('newsletter').down('emailfield').blur();
-                me.registerNewsletter(popup.down('newsletter').getValues(), 
-                    //remove on success
-                function() {
-                    Ext.Viewport.remove(popup);
-                });
-            }
+    //     //setup button handler
+    //     popup.on({
+    //         delegate: 'button[action=register]',
+    //         tap: function() {
+    //              //force keyboard to hide, due to a bug in Android 4.0 the textfield is visible above the popup
+    //             popup.down('newsletter').down('emailfield').blur();
+    //             me.registerNewsletter(popup.down('newsletter').getValues(), 
+    //                 //remove on success
+    //             function() {
+    //                 Ext.Viewport.remove(popup);
+    //             });
+    //         }
             
-        });
+    //     });
 
-        popup.on({
-            delegate: 'button[action=dont-ask]',
-            tap: function() {
-                appState.set('newsletterRegistered', true);
-               Ext.Viewport.remove(popup);
-            }
-        });
+    //     popup.on({
+    //         delegate: 'button[action=dont-ask]',
+    //         tap: function() {
+    //             appState.set('newsletterRegistered', true);
+    //            Ext.Viewport.remove(popup);
+    //         }
+    //     });
 
-        popup.on('hide', function() {
-             Ext.Viewport.remove(popup);
-        });
+    //     popup.on('hide', function() {
+    //          Ext.Viewport.remove(popup);
+    //     });
 
-        Ext.Viewport.add(popup);
-        popup.show();
+    //     Ext.Viewport.add(popup);
+    //     popup.show();
 
-    }
+    // }
 });
