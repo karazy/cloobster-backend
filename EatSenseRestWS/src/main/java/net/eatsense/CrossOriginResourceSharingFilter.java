7a62e4662ca5cec8163a0032c134b1a7070d6edc
Nils Weiher <weiher@karazy.net>
2012-06-19 14:55:45 +0200

    modified CORS filter

--- EatSenseRestWS/src/main/java/net/eatsense/CrossOriginResourceSharingFilter.java
+++ EatSenseRestWS/src/main/java/net/eatsense/CrossOriginResourceSharingFilter.java
@@ -11,11 +11,19 @@ public class CrossOriginResourceSharingFilter implements
 	public ContainerResponse filter(ContainerRequest request,
 			ContainerResponse response) {
 		String requestedHeader = request.getHeaderValue("Access-Control-Request-Headers");
+		if(requestedHeader == null)
+        	requestedHeader = "origin, x-requested-with";
+		
 		response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
 		response.getHttpHeaders().putSingle("Access-Control-Allow-Credentials", "true");
         response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", requestedHeader);
-
+        response.getHttpHeaders().putSingle("Access-Control-Max-Age", "1728000");
+        
+        if(request.getMethod().equals("OPTIONS")) {
+        	// This was an OPTIONS request, which should contain no data.
+        	response.setStatus(204);
+        }
         return response;
 	}
 }
