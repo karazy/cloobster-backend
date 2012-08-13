abcec7a52730ed9cae4caed1a523d814cb769814
Nils Weiher <weiher@karazy.net>
2012-08-13 15:46:59 +0200

    [touch:206] refactore requestDto creation, added receivedTime field.

--- EatSenseRestWS/src/main/java/net/eatsense/representation/CustomerRequestDTO.java
+++ EatSenseRestWS/src/main/java/net/eatsense/representation/CustomerRequestDTO.java
@@ -1,11 +1,34 @@
 package net.eatsense.representation;
 
+import java.util.Date;
+
+import net.eatsense.domain.Request;
+
 public class CustomerRequestDTO {
 	private String type;
 	private Long id;
 	
 	private Long checkInId;
 	private Long spotId;
+	
+	private Date receivedTime;
+	
+	public CustomerRequestDTO() {
+		super();
+	}
+	
+	/**
+	 * @param request Domain object to use as data source.
+	 */
+	public CustomerRequestDTO(Request request) {
+		this();
+		
+		this.setId(request.getId());
+		this.setCheckInId(request.getCheckIn().getId());
+		this.setSpotId(request.getSpot().getId());
+		this.setType(request.getStatus());
+		this.setReceivedTime(request.getReceivedTime());
+	}
 	
 	public String getType() {
 		return type;
@@ -37,6 +60,14 @@ public class CustomerRequestDTO {
 
 	public void setSpotId(Long spotId) {
 		this.spotId = spotId;
+	}
+
+	public Date getReceivedTime() {
+		return receivedTime;
+	}
+
+	public void setReceivedTime(Date receivedTime) {
+		this.receivedTime = receivedTime;
 	}
 	
 	
