53ebdfee1a8115b36aa1086fd71f5e2f0fc78c7a
Nils Weiher <weiher@karazy.net>
2012-09-11 12:32:43 +0200

    added missing profilesresource

---
+++ EatSenseRestWS/src/main/java/net/eatsense/restws/customer/ProfilesResource.java
@@ -0,0 +1,65 @@
+package net.eatsense.restws.customer;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import net.eatsense.auth.Role;
+import net.eatsense.controller.ProfileController;
+import net.eatsense.domain.Account;
+import net.eatsense.domain.CustomerProfile;
+import net.eatsense.exceptions.IllegalAccessException;
+import net.eatsense.representation.CustomerProfileDTO;
+
+@Path("c/profiles")
+@Produces("application/json; charset=utf-8")
+public class ProfilesResource {
+	private final HttpServletRequest servletRequest;
+	private final ProfileController profileCtrl;
+	private final Account account;
+	
+	public ProfilesResource(HttpServletRequest servletRequest,
+			ProfileController profileCtrl) {
+		super();
+		this.servletRequest = servletRequest;
+		this.account = (Account)servletRequest.getAttribute("net.eatsense.domain.Account");
+		this.profileCtrl = profileCtrl;
+	}
+	
+	/**
+	 * @param id
+	 * @return customer profile data
+	 */
+	@GET
+	@Path("{profileId}")
+	@RolesAllowed({Role.USER, Role.BUSINESSADMIN, Role.COMPANYOWNER})
+	public CustomerProfileDTO getProfile(@PathParam("profileId") long id){
+		if(account.getCustomerProfile().getId() != id) {
+			throw new IllegalAccessException("Can only read the own account's profile.");
+		}
+
+		return new CustomerProfileDTO(profileCtrl.get(CustomerProfile.getKey(id)));
+	}
+
+	/**
+	 * Update customer profile of an account.
+	 * 
+	 * @param id
+	 * @param profileData
+	 * @return updated profileData
+	 */
+	@PUT
+	@Path("{profileId}")
+	@RolesAllowed({Role.USER, Role.BUSINESSADMIN, Role.COMPANYOWNER})
+	public CustomerProfileDTO updateProfile(@PathParam("profileId") long id, CustomerProfileDTO profileData) {
+		if(account.getCustomerProfile().getId() != id) {
+			throw new IllegalAccessException("Can only update the own account's profile.");
+		}
+		
+		return new CustomerProfileDTO( profileCtrl.updateCustomerProfile(account.getCustomerProfile(), profileData));
+	}
+}
