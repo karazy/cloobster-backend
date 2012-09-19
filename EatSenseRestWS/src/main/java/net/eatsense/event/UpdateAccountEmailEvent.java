f063f1dcf1a6ba8b61862a3374d08a1f07ff4f82
Nils Weiher <weiher@karazy.net>
2012-09-19 14:26:18 +0200

    fixed update email message issues

--- EatSenseRestWS/src/main/java/net/eatsense/event/UpdateAccountEmailEvent.java
+++ EatSenseRestWS/src/main/java/net/eatsense/event/UpdateAccountEmailEvent.java
@@ -2,19 +2,35 @@ package net.eatsense.event;
 
 import javax.ws.rs.core.UriInfo;
 
+import com.google.common.base.Optional;
+
 import net.eatsense.domain.Account;
 
 public class UpdateAccountEmailEvent extends AccountEvent {
 
 	private final UriInfo uriInfo;
+	private final Optional<String> previousEmail;
 
 	public UpdateAccountEmailEvent(Account account, UriInfo uriInfo) {
 		super(account);
 		this.uriInfo = uriInfo;
+		
+		this.previousEmail = Optional.absent();
+	}
+	
+	public UpdateAccountEmailEvent(Account account, UriInfo uriInfo, String previousEmail) {
+		super(account);
+		this.uriInfo = uriInfo;
+		
+		this.previousEmail = Optional.fromNullable(previousEmail);
 	}
 
 	public UriInfo getUriInfo() {
 		return uriInfo;
+	}
+
+	public Optional<String> getPreviousEmail() {
+		return previousEmail;
 	}
 
 }
