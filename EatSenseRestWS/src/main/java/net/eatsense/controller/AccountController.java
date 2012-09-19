7d238cfa797b2d78630b4a0de88ef1051fd2784b
Nils Weiher <weiher@karazy.net>
2012-09-19 13:19:16 +0200

    changed admin template management

--- EatSenseRestWS/src/main/java/net/eatsense/controller/AccountController.java
+++ EatSenseRestWS/src/main/java/net/eatsense/controller/AccountController.java
@@ -643,9 +643,11 @@ public class AccountController {
 				checkEmailDoesNotExist(accountData.getEmail());
 				if(!emailConfirmed) {
 					account.setNewEmail(accountData.getEmail());
+					
 				}
 				else {
 					account.setEmail(accountData.getEmail());
+					logger.info("Writing new e-mail address {}", account.getEmail());
 				}
 				
 				account.setEmailConfirmed(false);
