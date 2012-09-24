8785b929bdbd327d708da66d44c0e08c3e24d9fa
Nils Weiher <weiher@karazy.net>
2012-09-24 10:34:29 +0200

    first draft of bill creation from cockpit

--- EatSenseRestWS/src/main/java/net/eatsense/restws/business/BillsResource.java
+++ EatSenseRestWS/src/main/java/net/eatsense/restws/business/BillsResource.java
@@ -3,12 +3,15 @@ package net.eatsense.restws.business;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+
+import org.quartz.xml.ValidationException;
 
 import net.eatsense.auth.Role;
 import net.eatsense.controller.BillController;
@@ -45,6 +48,20 @@ public class BillsResource {
 			throw new NotFoundException();
 		return billData;
 	}
+	
+	@POST
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@RolesAllowed({Role.COCKPITUSER, Role.BUSINESSADMIN, Role.COMPANYOWNER})
+	public BillDTO createBill(BillDTO billData) {
+		if(billData.getCheckInId() == null) {
+			throw new net.eatsense.exceptions.ValidationException("checkInId was null", "validationError");
+			
+		}
+		//TODO finish method
+		return null;//billController.createBill(business, checkIn, billData);
+	}
+	
 	
 	@Path("{id}")
 	@PUT
