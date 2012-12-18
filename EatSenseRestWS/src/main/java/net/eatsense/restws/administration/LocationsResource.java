0218fc313cbbf95fa19c776bae7a59ab6dbb68a1
Nils Weiher <weiher@karazy.net>
2012-12-18 14:52:53 +0100

    added resource method for adding new subscribtions to business

--- EatSenseRestWS/src/main/java/net/eatsense/restws/administration/LocationsResource.java
+++ EatSenseRestWS/src/main/java/net/eatsense/restws/administration/LocationsResource.java
@@ -3,6 +3,9 @@ package net.eatsense.restws.administration;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
@@ -10,20 +13,31 @@ import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 import net.eatsense.controller.LocationController;
+import net.eatsense.controller.SubscriptionController;
+import net.eatsense.domain.Business;
 import net.eatsense.representation.LocationProfileDTO;
+import net.eatsense.representation.SubscriptionDTO;
 
 public class LocationsResource {
 	private final LocationController ctrl;
+	private final SubscriptionController subCtrl;
 
 	@Inject
-	public LocationsResource(LocationController ctrl) {
+	public LocationsResource(LocationController ctrl, SubscriptionController subCtrl) {
 		super();
 		this.ctrl = ctrl;
+		this.subCtrl = subCtrl;
 	}
 	
 	@GET
 	@Produces("application/json")
 	public List<LocationProfileDTO> getLocations(@QueryParam("id") long companyId) {
 		return Lists.transform(ctrl.getLocations(companyId), LocationProfileDTO.toDTO);
+	}
+	
+	@POST
+	@Path("{locationId}/subscriptions")
+	public SubscriptionDTO createNewSubscription(@PathParam("locationId") long locationId,SubscriptionDTO subscriptionData) {
+		return new SubscriptionDTO(subCtrl.createSubscriptionFromTemplate(subCtrl.getPackage(subscriptionData.getTemplateId()), subscriptionData.getStatus(), Business.getKey(locationId)));
 	}
 }
