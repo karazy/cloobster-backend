51b7b60546253c8b7ac615475ab8e5d9a707d7f5
Nils Weiher <weiher@karazy.net>
2013-02-14 17:25:12 +0100

    Added reportcontroller

--- EatSenseRestWS/src/main/java/net/eatsense/controller/ReportController.java
+++ EatSenseRestWS/src/main/java/net/eatsense/controller/ReportController.java
@@ -1,5 +1,18 @@
 package net.eatsense.controller;
 
+import com.google.inject.Inject;
+
+import net.eatsense.counter.CounterRepository;
+
 public class ReportController {
+	
+	private final CounterRepository counterRepo;
+
+	@Inject
+	public ReportController(CounterRepository counterRepo) {
+		super();
+		this.counterRepo = counterRepo;
+	}
+	
 	
 }
