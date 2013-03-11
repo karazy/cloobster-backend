eeeb6051b93f7722fb3c36a3f051962be88acb3e
Nils Weiher <weiher@karazy.net>
2013-03-11 14:46:44 +0100

    Added default value for ApiVersion

--- EatSenseRestWS/src/main/java/net/eatsense/filter/annotation/ApiVersion.java
+++ EatSenseRestWS/src/main/java/net/eatsense/filter/annotation/ApiVersion.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
 @Retention (RUNTIME)
 @Target({TYPE, METHOD})
 public @interface ApiVersion {
-	int[] value();
+	int[] value() default {};
 	int min() default 0;
 }
