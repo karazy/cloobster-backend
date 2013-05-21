4eb5f5d0625f56e479c2f7a44616231c2c76f8c7
Nils Weiher <weiher@karazy.net>
2013-05-21 13:45:18 +0200

    created Visit entity

---
+++ EatSenseRestWS/src/main/java/net/eatsense/domain/Visit.java
@@ -0,0 +1,14 @@
+package net.eatsense.domain;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Parent;
+
+public class Visit extends GenericEntity<Visit> {
+	
+	@Override
+	public Key<Visit> getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
