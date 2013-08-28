24237603965eb5d42d99b3efc4c3b146cae767f2
Frederik Reifschneider <frederik.reifschneider@gmail.com>
2013-08-28 15:33:51 +0200

    added configuration to company

--- EatSenseRestWS/src/main/java/net/eatsense/controller/CompanyController.java
+++ EatSenseRestWS/src/main/java/net/eatsense/controller/CompanyController.java
@@ -1,10 +1,21 @@
 package net.eatsense.controller;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import net.eatsense.configuration.addon.AddonConfiguration;
+import net.eatsense.configuration.addon.AddonConfigurationService;
+import net.eatsense.domain.Business;
 import net.eatsense.domain.Company;
+import net.eatsense.exceptions.ValidationException;
 import net.eatsense.persistence.CompanyRepository;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +24,14 @@ import com.googlecode.objectify.NotFoundException;
 
 public class CompanyController {
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
-	private final CompanyRepository repo;
+	private final CompanyRepository repo;	
+	private final AddonConfigurationService addonConfig;
 	
 	@Inject
-	public CompanyController(CompanyRepository repo) {
+	public CompanyController(CompanyRepository repo,  AddonConfigurationService addonConfig) {
 		super();
 		this.repo = repo;
+		this.addonConfig = addonConfig;
 	}
 	
 	/**
@@ -40,5 +53,43 @@ public class CompanyController {
 	 */
 	public Collection<Company> getAll() {
 		return repo.getAll();
+	}
+	
+	/**
+	 * Get configuration.
+	 * @param name
+	 * @return
+	 */
+	public Map<String, String> getConfiguration(Company company, String name) {		
+		return addonConfig.get(name, company.getKey().getRaw()).getConfigMap();
+	}
+	
+
+	/**
+	 * Save configuration.
+	 * @param company
+	 * @param name
+	 * @param configMap
+	 * @return
+	 */
+	public Map<String, String> saveConfiguration(Company company,
+			String name, JSONObject configMap) {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		HashMap<String, String>  _map = null;
+		try {
+			_map = mapper.readValue(configMap.toString(), HashMap.class);
+		} catch (JsonParseException e1) {
+			throw new ValidationException("Could not parse json configuration. " + e1.toString());
+		} catch (JsonMappingException e1) {
+			throw new ValidationException("Could not parse json configuration. " + e1.toString());
+		} catch (IOException e1) {
+			throw new ValidationException("Could not parse json configuration. " + e1.toString());
+		}
+		
+		AddonConfiguration config = addonConfig.create(name, company.getKey().getRaw(), _map);
+		addonConfig.put(config);
+		
+		return config.getConfigMap();
 	}
 }
--- EatSenseRestWS/src/main/java/net/eatsense/restws/administration/CompaniesResource.java
+++ EatSenseRestWS/src/main/java/net/eatsense/restws/administration/CompaniesResource.java
@@ -1,16 +1,24 @@
 package net.eatsense.restws.administration;
 
 import java.util.Collection;
+import java.util.Map;
 
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.codehaus.jettison.json.JSONObject;
+
 import com.google.common.collect.Collections2;
 import com.google.inject.Inject;
 
+import net.eatsense.auth.Role;
 import net.eatsense.controller.CompanyController;
+import net.eatsense.domain.Company;
 import net.eatsense.representation.CompanyDTO;
 
 public class CompaniesResource {
@@ -33,5 +41,22 @@ public class CompaniesResource {
 	@Produces("application/json")
 	public CompanyDTO get(@PathParam("id")long id) {
 		return new CompanyDTO(ctrl.get(id));
+	}
+	
+	@GET
+	@Produces("application/json; charset=UTF-8")
+	@Path("{id}/configurations/{name}")
+	public Map<String, String> getConfiguration(@PathParam("id")long id, @PathParam("name") String name){
+		Company company = ctrl.get(id); 
+		return ctrl.getConfiguration(company, name);
+	}
+	
+	@PUT
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@Path("{id}/configurations/{name}")
+	public Map<String, String> saveConfiguration(@PathParam("id")long id, @PathParam("name") String name, JSONObject configMap){
+		Company company = ctrl.get(id); 
+		return ctrl.saveConfiguration(company, name, configMap); 
 	}
 }
